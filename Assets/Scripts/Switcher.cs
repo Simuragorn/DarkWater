b0c7e222472249d59e155914837ffbe0c1b2fab2
Bogdan.G <bogdan.g@solity.io>
2024-07-07 14:35:11 +0300

    Add smooth gears changing with delay for submarine motor

--- Assets/Scripts/Switcher.cs
+++ Assets/Scripts/Switcher.cs
@@ -1,13 +1,19 @@
-using Assets.Scripts.Consts;
 using Assets.Scripts.Helpers;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider2D))]
 public class Switcher : MonoBehaviour
 {
+    public enum GearEnum
+    {
+        Highest,
+        Neutral,
+        Lowest
+    }
+
     [SerializeField] private float xOffset = 1f;
-    [SerializeField] private float neutralGearDelay = 0.5f;
-    [SerializeField] private float neutralGearSupportDelay = 2f;
+    [SerializeField] private float gearDelay = 0.5f;
+    [SerializeField] private float gearSupportDelay = 2f;
     [SerializeField] private float gearCheckingOffset = 0.001f;
 
     private float localXStartPosition;
@@ -17,8 +23,9 @@ public class Switcher : MonoBehaviour
     private Collider2D collider;
     private Rigidbody2D rigidbody;
     private RigidbodyConstraints2D defaultRigidbodyContraints;
-    private float neutralGearDelayLeft = 0;
-    private float neutralGearSupportDelayLeft = 0;
+    private float gearDelayLeft = 0;
+    private float gearSupportDelayLeft = 0;
+    private GearEnum? previousGearPosition;
 
 
     /// <summary>
@@ -43,14 +50,37 @@ public class Switcher : MonoBehaviour
         float xPosition = transform.localPosition.x;
         xPosition = Mathf.Clamp(xPosition, minLocalXPosition, maxLocalXPosition);
         switchValue = MathHelper.MapValue(xPosition, minLocalXPosition, maxLocalXPosition, -1, 1);
+
+        previousGearPosition = GetCurrentGear();
+    }
+
+    private GearEnum? GetCurrentGear()
+    {
+        Vector2 localPosition = transform.localPosition;
+        float distanceToCenter = Mathf.Abs(localPosition.x - localXStartPosition);
+        float distanceToLeft = Mathf.Abs(localPosition.x - minLocalXPosition);
+        float distanceToRight = Mathf.Abs(localPosition.x - maxLocalXPosition);
+        if (distanceToCenter <= gearCheckingOffset)
+        {
+            return GearEnum.Neutral;
+        }
+        if (distanceToLeft <= gearCheckingOffset)
+        {
+            return GearEnum.Highest;
+        }
+        if (distanceToRight <= gearCheckingOffset)
+        {
+            return GearEnum.Lowest;
+        }
+        return null;
     }
 
     private void LimitMovement()
     {
         Vector2 localPosition = transform.localPosition;
-        localPosition = HandleNeutralGear(localPosition);
+        localPosition = HandleGearChanging(localPosition);
 
-        if (neutralGearDelayLeft > 0)
+        if (gearDelayLeft > 0)
         {
             return;
         }
@@ -65,22 +95,40 @@ public class Switcher : MonoBehaviour
         }
     }
 
-    private Vector2 HandleNeutralGear(Vector2 localPosition)
+    private Vector2 HandleGearChanging(Vector2 localPosition)
     {
-        neutralGearDelayLeft = Mathf.Max(0, neutralGearDelayLeft - Time.deltaTime);
-        neutralGearSupportDelayLeft = Mathf.Max(0, neutralGearSupportDelayLeft - Time.deltaTime);
+        gearDelayLeft = Mathf.Max(0, gearDelayLeft - Time.deltaTime);
+        gearSupportDelayLeft = Mathf.Max(0, gearSupportDelayLeft - Time.deltaTime);
 
-        float distanceToCenter = Mathf.Abs(localPosition.x - localXStartPosition);
-        if (rigidbody.velocity.x != 0 && distanceToCenter <= gearCheckingOffset && neutralGearSupportDelayLeft <= 0)
+        GearEnum? currentGear = GetCurrentGear();
+        if (rigidbody.velocity.x != 0 && currentGear != null && previousGearPosition != currentGear && gearSupportDelayLeft <= 0)
         {
-            localPosition.x = localXStartPosition;
+            string gearText = "unknown gear";
+            switch (currentGear.Value)
+            {
+                case GearEnum.Highest:
+                    localPosition.x = minLocalXPosition;
+                    gearText = "highest gear";
+                    break;
+                case GearEnum.Neutral:
+                    localPosition.x = localXStartPosition;
+                    gearText = "neutral gear";
+                    break;
+                case GearEnum.Lowest:
+                    localPosition.x = maxLocalXPosition;
+                    gearText = "lowest gear";
+                    break;
+                default:
+                    break;
+            }
             transform.localPosition = localPosition;
 
             rigidbody.constraints = defaultRigidbodyContraints | RigidbodyConstraints2D.FreezePositionX;
-            neutralGearDelayLeft = neutralGearDelay;
-            neutralGearSupportDelayLeft = neutralGearSupportDelay;
+            gearDelayLeft = gearDelay;
+            gearSupportDelayLeft = gearSupportDelay;
+            Debug.Log(gearText);
         }
-        else if (neutralGearDelayLeft <= 0)
+        else if (gearDelayLeft <= 0)
         {
             rigidbody.constraints = defaultRigidbodyContraints;
         }
