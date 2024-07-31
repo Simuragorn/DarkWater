9f4bfc65c7bdede6a7b94a8129da5fd1690c91f6
Bogdan.G <bogdan.g@solity.io>
2024-07-31 22:43:41 +0300

    Add menu options to the terminal

--- Assets/Scripts/Terminal.cs
+++ Assets/Scripts/Terminal.cs
@@ -1,9 +1,20 @@
 using System;
+using TMPro;
 using UnityEngine;
 
 public class Terminal : PluggableObject
 {
     [SerializeField] private Transform terminalPanel;
+    [SerializeField] private TextMeshProUGUI terminalText;
+    [SerializeField] private TMP_InputField terminalInput;
+    private bool isActive => terminalPanel.gameObject.activeInHierarchy;
+
+    private const string TasksMenuName = "Tasks";
+    private const string BestiaryMenuName = "Bestiary";
+    private const string RadarMenuName = "Radar";
+    private const string InfoMenuName = "Info";
+    private const string MenuName = "Menu";
+    private const string MenuQuickName = "Q";
 
     private void Awake()
     {
@@ -12,11 +23,67 @@ public class Terminal : PluggableObject
 
     void Update()
     {
+        if (!isActive)
+        {
+            return;
+        }
+        HandleClosing();
+        HandleMenuChanging();
+    }
+
+    private void HandleMenuChanging()
+    {
+        string userInput = terminalInput.text;
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (string.IsNullOrWhiteSpace(userInput))
+            {
+                terminalInput.ActivateInputField();
+                return;
+            }
+            userInput = userInput.Trim();
+            if (userInput.Equals(TasksMenuName, StringComparison.InvariantCultureIgnoreCase))
+            {
+                terminalText.text = "No current tasks";
+            }
+            else if (userInput.Equals(BestiaryMenuName, StringComparison.InvariantCultureIgnoreCase))
+            {
+                terminalText.text = "List of beasts: ...";
+            }
+            else if (userInput.Equals(RadarMenuName, StringComparison.InvariantCultureIgnoreCase))
+            {
+                terminalText.text = "Trying to restart radar";
+            }
+            else if (userInput.Equals(InfoMenuName, StringComparison.InvariantCultureIgnoreCase))
+            {
+                terminalText.text = @"You are 100 meters below sea level.";
+            }
+            else if (userInput.Equals(MenuName, StringComparison.InvariantCultureIgnoreCase) || userInput.Equals(MenuQuickName, StringComparison.InvariantCultureIgnoreCase))
+            {
+                terminalText.text = @"
+Menu:
+- Tasks
+- Bestiary
+- Radar
+- Info";
+            }
+            else
+            {
+                terminalText.text = "Unknown command";
+            }
+            terminalInput.text = string.Empty;
+            terminalInput.ActivateInputField();
+        }
+    }
+
+    private void HandleClosing()
+    {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             CloseTerminal();
         }
     }
+
     protected override void Slot_OnPowerDisconnected(object sender, EventArgs e)
     {
         base.Slot_OnPowerDisconnected(sender, e);
@@ -41,5 +108,6 @@ public class Terminal : PluggableObject
     private void OpenTerminal()
     {
         terminalPanel.gameObject.SetActive(true);
+        terminalInput.ActivateInputField();
     }
 }
